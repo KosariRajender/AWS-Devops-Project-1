69e3810af7447639fd4d0b9e212ae553c9bb04e9
KosariRajender <157151918+KosariRajender@users.noreply.github.com>
2024-08-23 22:43:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "myec2" {
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = file("./terraform-key.pem")
+    private_key = data.aws_key_pair.ec2key
     host        = self.public_ip
   }
   # here iam using provisioner to execute commands in the server
