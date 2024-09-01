0d68be6fa98f754182c71c75f0ae331c4db2c782
KosariRajender <157151918+KosariRajender@users.noreply.github.com>
2024-09-01 17:02:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -15,3 +15,15 @@ output "ec2_instance_public_ip" {
   description = "The public IP address of the EC2 instance"
   value       = aws_instance.myec2.public_ip
 }
+# creating linux server for nginx 
+resource "aws_instance" "myec22" {
+  ami           = "ami-0ae8f15ae66fe8cda"
+  instance_type = "t2.micro"
+  key_name       = data.aws_key_pair.ec2key.key_name
+
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+
+  tags = {
+    Name = "terraformtest"
+  }
+}
